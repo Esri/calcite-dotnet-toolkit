f3a856504a9eb013afba4c11c1024eba7023768c
Morten Nielsen <mort5161@esri.com>
2024-07-24 12:47:22 -0700

    Fix maui styling on Windows

--- src/Esri.Calcite.Maui/AppHostBuilderExtensions.cs
+++ src/Esri.Calcite.Maui/AppHostBuilderExtensions.cs
@@ -1,4 +1,7 @@
-﻿[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://schemas.esri.com/calcite/2024", "calcite")]
+﻿using Microsoft.Maui.LifecycleEvents;
+using Microsoft.Maui.Handlers;
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://schemas.esri.com/calcite/2024", "calcite")]
 [assembly: XmlnsDefinition("http://schemas.esri.com/calcite/2024", "Esri.Calcite.Maui")]
 
 namespace Esri.Calcite.Maui
@@ -14,16 +17,44 @@ namespace Esri.Calcite.Maui
       /// <returns>The host builder</returns>
         public static MauiAppBuilder UseCalcite(this MauiAppBuilder builder)
         {
-            builder.ConfigureMauiHandlers(delegate (IMauiHandlersCollection a)
+            builder = builder.ConfigureMauiHandlers(delegate (IMauiHandlersCollection a)
             {
             }).ConfigureFonts(fonts =>
-                 fonts.AddEmbeddedResourceFont(typeof(AppHostBuilderExtensions).Assembly, "calcite-ui-icons.ttf", "CalciteUIIconsFontFamily")
-                      //.AddFont("AvenirNextW1G-Demi.otf", "AvenirNextW1G-Demi")
-                      //.AddFont("AvenirNextW1G-Light.otf", "AvenirNextW1G-Light")
-                      //.AddFont("AvenirNextW1G-Medium.otf", "AvenirNextW1G-Medium")
-                      //.AddFont("AvenirNextW1G-Regular.otf", "AvenirNextW1G-Regular")
-                 ) ;//.ConfigureImageSources(;
-            
+                 fonts.AddEmbeddedResourceFont(typeof(AppHostBuilderExtensions).Assembly, "calcite-ui-icons.ttf", "CalciteUIIconsFontFamily");
+#if WINDOWS
+            builder = builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(w =>
+                {
+                    w.OnLaunching((app, e) =>
+                    {
+                        var xamlresources = app.Resources.MergedDictionaries.OfType<Microsoft.UI.Xaml.Controls.XamlControlsResources>().FirstOrDefault();
+                        if(xamlresources is not null)
+                        {
+                            xamlresources.MergedDictionaries.Add(new Esri.Calcite.WinUI.CalciteResources());
+                        }
+                      
+                    });
+                });
+            });
+#endif
+
+            /*RadioButtonHandler.Mapper.AppendToMapping("RadioButtonStyleOverride", (handler, view) =>
+            {
+#if WINDOWS
+                // Temporary workaround for radiobutton on Windows
+                // Note: This won't support resource overrides or theme changes
+                var CalciteUIBrandLightColor = Windows.UI.Color.FromArgb(255, 0, 122, 194);
+                var CalciteUIBrandHoverLightColor = Windows.UI.Color.FromArgb(255, 0, 97, 155);
+                var CalciteUIBrandPressLightColor = Windows.UI.Color.FromArgb(255, 0, 72, 116);
+                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFillPressed"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandPressLightColor);
+                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStrokePressed"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandPressLightColor);
+                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFillPointerOver"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandHoverLightColor);
+                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFill"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandLightColor);
+                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStrokePointerOver"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandHoverLightColor);
+                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStroke"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandHoverLightColor);
+#endif
+            });*/
             return builder;
         }
     }
