718c9cef516b411e9846b881eee1aca973e3831b
dotMorten <mn@iter.dk>
2024-11-18 15:20:13 -0800

    Remove dependency on WinUI and allow brand brush override

--- src/Esri.Calcite.Maui/AppHostBuilderExtensions.cs
+++ src/Esri.Calcite.Maui/AppHostBuilderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maui.LifecycleEvents;
 using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
 
 [assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://schemas.esri.com/calcite/2024", "calcite")]
 [assembly: XmlnsDefinition("http://schemas.esri.com/calcite/2024", "Esri.Calcite.Maui")]
@@ -28,40 +29,31 @@ namespace Esri.Calcite.Maui
                  fonts.AddEmbeddedResourceFont(typeof(AppHostBuilderExtensions).Assembly, "calcite-ui-icons-16.ttf", CalciteUIIconsSmallFontFamily)
                       .AddEmbeddedResourceFont(typeof(AppHostBuilderExtensions).Assembly, "calcite-ui-icons-24.ttf", CalciteUIIconsMediumFontFamily)
                       .AddEmbeddedResourceFont(typeof(AppHostBuilderExtensions).Assembly, "calcite-ui-icons-32.ttf", CalciteUIIconsLargeFontFamily));
-#if WINDOWS
-            builder = builder.ConfigureLifecycleEvents(events =>
-            {
-                events.AddWindows(w =>
-                {
-                    w.OnLaunching((app, e) =>
-                    {
-                        var xamlresources = app.Resources.MergedDictionaries.OfType<Microsoft.UI.Xaml.Controls.XamlControlsResources>().FirstOrDefault();
-                        if(xamlresources is not null)
-                        {
-                            xamlresources.MergedDictionaries.Add(new Esri.Calcite.WinUI.CalciteResources());
-                        }
-                      
-                    });
-                });
-            });
-#endif
 
-            /*RadioButtonHandler.Mapper.AppendToMapping("RadioButtonStyleOverride", (handler, view) =>
+            RadioButtonHandler.Mapper.AppendToMapping("RadioButtonStyleOverride", (handler, view) =>
             {
 #if WINDOWS
-                // Temporary workaround for radiobutton on Windows
-                // Note: This won't support resource overrides or theme changes
-                var CalciteUIBrandLightColor = Windows.UI.Color.FromArgb(255, 0, 122, 194);
-                var CalciteUIBrandHoverLightColor = Windows.UI.Color.FromArgb(255, 0, 97, 155);
-                var CalciteUIBrandPressLightColor = Windows.UI.Color.FromArgb(255, 0, 72, 116);
-                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFillPressed"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandPressLightColor);
-                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStrokePressed"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandPressLightColor);
-                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFillPointerOver"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandHoverLightColor);
-                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFill"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandLightColor);
-                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStrokePointerOver"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandHoverLightColor);
-                handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStroke"] = new Microsoft.UI.Xaml.Media.SolidColorBrush(CalciteUIBrandHoverLightColor);
+                // Temporary workaround for radiobutton styling limitation on Windows
+                if (view is BindableObject bo)
+                {
+                    if (CalciteResourceHelper.GetColor(bo) is Color c)
+                    {
+                        handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFill"] =
+                        handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStroke"] = c.ToPlatform();
+                    }
+                    if (CalciteResourceHelper.GetHoverColor(bo) is Color pressedColor)
+                    {
+                        handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFillPressed"] =
+                        handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStrokePressed"] = pressedColor.ToPlatform();
+                    }
+                    if (CalciteResourceHelper.GetPressedColor(bo) is Color hoverColor)
+                    {
+                        handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedFillPointerOver"] =
+                        handler.PlatformView.Resources["RadioButtonOuterEllipseCheckedStrokePointerOver"] = hoverColor.ToPlatform();
+                    }
+                }
 #endif
-            });*/
+            });
             return builder;
         }
     }
---
+++ src/Esri.Calcite.Maui/CalciteResourceHelper.cs
@@ -0,0 +1,50 @@
+﻿namespace Esri.Calcite.Maui
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    [System.ComponentModel.Browsable(false)]
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    public static class CalciteResourceHelper
+    {
+        public static Color GetColor(BindableObject obj)
+        {
+            return (Color)obj.GetValue(ColorProperty);
+        }
+
+        public static void SetColor(BindableObject obj, Color value)
+        {
+            obj.SetValue(ColorProperty, value);
+        }
+
+        public static readonly BindableProperty ColorProperty =
+            BindableProperty.CreateAttached("Color", typeof(Color), typeof(CalciteResourceHelper), null);
+
+
+        public static Color GetHoverColor(BindableObject obj)
+        {
+            return (Color)obj.GetValue(HoverColorProperty);
+        }
+
+        public static void SetHoverColor(BindableObject obj, Color value)
+        {
+            obj.SetValue(HoverColorProperty, value);
+        }
+
+        public static readonly BindableProperty HoverColorProperty =
+            BindableProperty.CreateAttached("HoverColor", typeof(Color), typeof(CalciteResourceHelper), null);
+
+
+        public static Color GetPressedColor(BindableObject obj)
+        {
+            return (Color)obj.GetValue(PressedColorProperty);
+        }
+
+        public static void SetPressedColor(BindableObject obj, Color value)
+        {
+            obj.SetValue(PressedColorProperty, value);
+        }
+
+        public static readonly BindableProperty PressedColorProperty =
+            BindableProperty.CreateAttached("PressedColor", typeof(Color), typeof(CalciteResourceHelper), null);
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
