6211a35c709203f76dfc95b3a734aefd5650a45b
Morten Nielsen <mort5161@esri.com>
2024-11-19 15:07:23 -0800

    WPF: Fix window and menu styling

--- src/TestApps/WpfTests/MainWindow.xaml.cs
+++ src/TestApps/WpfTests/MainWindow.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using Esri.Calcite.WPF;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfTests
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -72,6 +58,5 @@ namespace WpfTests
                 ParentTabControl.Items.Add(item);
             }
         }
-
     }
 }
