668aca46669b8e229c93c63f54923f76ca499239
dotMorten <mn@iter.dk>
2025-02-21 17:18:13 -0800

    Add missing copyright headers

--- src/Esri.Calcite.WinUI/CalciteIconScale.cs
+++ src/Esri.Calcite.WinUI/CalciteIconScale.cs
@@ -1,4 +1,14 @@
-﻿namespace Esri.Calcite.WinUI
+﻿// COPYRIGHT © 2025 Esri
+// All rights reserved under the copyright laws of the United States and applicable international laws, treaties, and conventions.
+// This material is licensed for use under the Esri Master License Agreement (MLA), and is bound by the terms of that agreement.
+// You may redistribute and use this code without modification, provided you adhere to the terms of the MLA and include this copyright notice.
+//
+// See use restrictions at http://www.esri.com/legal/pdfs/mla_e204_e300/english
+//
+// For additional information, contact: Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department 380 New York Street Redlands, California, USA 92373 USA. email: contracts@esri.com
+
+namespace Esri.Calcite.WinUI
 {
     /// <summary>
     /// Calcite Icon scale. Why 3 Sizes? More info on what happens when you scale vector based icons <a href="https://github.com/Esri/calcite-ui-icons/wiki/What-Happens-When-You-Scale-Vector-Based-Icons">here</a>.
